d4194a0023d52d0e1715a2fc32b3d52f3ffa4084
Patrick Fuller <patrickfuller@gmail.com>
2013-12-26 23:10:39 -0500

    Added in digraph support and node sizing

--- README.md
+++ README.md
@@ -1,73 +1,83 @@
 igraph
 ======
 
-An interactive 3D graph visualizer for modern browsers. Check out the demo
-[here](http://www.patrick-fuller.com/igraph/example/).
+An interactive 3D graph visualizer for modern browsers.
+http://patrickfuller.github.io/igraph/
 
-For publication-quality renderings of 3D graphs, check out the [blender-graphs](https://github.com/patrickfuller/blender-graphs) project.
+Examples
+========
 
-![](http://imgur.com/98C5eoJ.png)
+ * [les misérables](http://patrickfuller.github.io/igraph/examples/miserables.html)
+ * [github connections](http://patrickfuller.github.io/igraph/examples/github.html)
 
 Usage
 =====
 
-```
+```javascript
 igraph.create('my-selector');
 igraph.draw(myGraph);
 ```
 
-where `'my-selector'` is where you want to place igraph, and `myGraph` is
-an object. See below for more on the object structure, or just check out the
-included example. The `igraph.create()` method takes a few optional parameters, specifying the
-sizes and colors of nodes, as well as force-directed optimization. Read the source for more.
+where `'my-selector'` is where you want to place igraph, and `myGraph` is a plain ol' object. See below for more on the object structure, or just check out the included example. The `igraph.create()` method takes a few optional parameters, specifying the sizes and colors of nodes, as well as force-directed optimization.
+
+```javascript
+options = {
+    directed: true, // Toggles edge arrows
+    nodeSize: 2.0, // Default node size
+    edgeSize: 0.25, // Edge connection diameter
+    arrowSize: 1.0, // If drawn, edge arrow size
+    defaultNodeColor: "0xaaaaaa", // Color for nodes without a "color" property
+    defaultEdgeColor: "0x777777", // Color for edges without a "color" property
+    shader: THREE.ShaderToon.toon2, // three.js shader algorithm to use
+    runOptimization: true // Runs a force-directed-layout algorithm on the graph
+};
+```
+
+Graph Data Format
+=================
+
+The viewers take input graph data structures as javascript objects. Here's the most boring graph in the world:
+
+```javascript
+{
+    nodes: {
+        jane: { },
+        bob: { },
+        mike: { },
+        sally: { }
+    },
+    edges: [
+        { source: "jane", target: "bob" },
+        { source: "bob", target: "mike" },
+        { source: "mike", target: "sally" }
+    ]
+}
+```
+
+Nodes require no information outside of their keys. However, there are useful optional parameters that can be specified.
+
+```javascript
+{
+    color: "0xffffff", // Color for this node
+    size: 1.0, // Scaling factor for this node's size
+    location: [0.0, 0.0, 0.0] // Starting location of node. Useful for pre-rendering.
+}
+```
+
+By default, the algorithm runs a force-directed layout on the graph. When enabled, the "location" field is optional. However, for larger graphs, you will want to disable this feature and pre-render the locations. Use the associated Python script to do so.
 
 IPython support
 ===============
 
 The IPython notebook is an open-source tool poised to replace MATLAB in many
-applications. As a scientist (of sorts), I'm all about it. Therefore, I made
+applications. As a scientist of sorts, I'm all about it. Therefore, I made
 handles to use igraph with the notebook.
 
-Open a new notebook with `ipython notebook` and make sure that the `igraph`
-directory is either in the directory you started the notebook or your
-PYTHONPATH. You can test the setup by typing:
+Open a new notebook and make sure that the igraph directory is either in the directory you started the notebook or your PYTHONPATH. You can test the setup by typing:
 
 ```python
 import igraph
 igraph.draw([(1, 2), (2, 3), (3, 4), (4, 1), (4, 5), (5, 2)])
 ```
 
-into a notebook cell. You should get a paddlewheel graph as an output. You can
-use this in conjunction with other code for educational purposes. Try
-generating a red-black tree! There are three commands and some optional
-parameters to check out. Read the docstrings for more.
-
-Graph Data Format
-=================
-
-The viewers take input graph data structures as javascript objects. As an
-example, consider:
-
-```
-{
-    "nodes": {
-        "jane": { },
-        "bob": { "location": [ -1.115, -2.167, -3.103 ], "color": "0x0000ff" },
-        "mike": { "color": "0xff0000" },
-        "sally": { "location": [ 3.348, 6.252, 8.937 ] }
-    },
-    "edges": [
-        { "source": "jane", "target": "bob" },
-        { "source": "bob", "target": "mike" },
-        { "source": "mike", "target": "sally" }
-    ]
-}
-```
-
-Nodes and edges can be colored by specifying a `"color"` field. If not specified,
-the visualizer defaults to grayscale (this behavior can be changed).
-
-By default, the algorithm runs a force-directed layout on the graph. When enabled,
-the `"location"` field is optional. However, for larger graphs, you will want to
-disable this feature and pre-render the locations. Use the associated Python
-script to do so.
+into a notebook cell. You should get a paddlewheel graph as an output. You can use this in conjunction with other code for educational purposes (try generating a red-black tree!). There are three commands and some optional parameters to check out. Read the docstrings for more.
--- igraph.js
+++ igraph.js
@@ -10,8 +10,10 @@ var igraph = {
         self = this;
         options = options || {};
 
+        this.directed = options.hasOwnProperty("directed") ? options.directed : true;
         this.nodeSize = options.hasOwnProperty("nodeSize") ? options.nodeSize : 2.0;
         this.edgeSize = options.hasOwnProperty("edgeSize") ? options.edgeSize : 0.25;
+        this.arrowSize = options.hasOwnProperty("arrowSize") ? options.arrowSize : this.edgeSize * 4;
         this.defaultNodeColor = options.hasOwnProperty("defaultNodeColor") ?
                 options.defaultNodeColor : "0xaaaaaa";
         this.defaultEdgeColor = options.hasOwnProperty("defaultEdgeColor") ?
@@ -28,13 +30,15 @@ var igraph = {
 
         this.controls = new THREE.TrackballControls(this.camera, this.renderer.domElement);
 
-        this.sphereGeometry = new THREE.SphereGeometry(1, 16, 12);
-        this.cylinderGeometry = new THREE.CylinderGeometry(1, 1, 1, 6, 3, false);
+        this.sphereGeometry = new THREE.SphereGeometry(this.nodeSize, 16, 12);
+        this.cylinderGeometry = new THREE.CylinderGeometry(this.edgeSize, this.edgeSize, 1, 32, 3, false);
+        this.coneGeometry = new THREE.CylinderGeometry(this.edgeSize, this.arrowSize, 2 * this.arrowSize, 32, 3, false);
 
         // This orients the cylinder primitive so THREE.lookAt() works properly
         vPreRotation = new THREE.Vector3(Math.PI / 2, Math.PI, 0);
         matrix = new THREE.Matrix4().makeRotationFromEuler(vPreRotation);
         this.cylinderGeometry.applyMatrix(matrix);
+        this.coneGeometry.applyMatrix(matrix);
 
         this.light = new THREE.HemisphereLight(0xffffff, 1.0);
         this.light.position = this.camera.position;
@@ -42,6 +46,7 @@ var igraph = {
 
         this.nodes = [];
         this.edges = [];
+        this.arrows = [];
         this.materials = {};
         this.materials[this.defaultNodeColor] = this.makeMaterial(this.defaultNodeColor);
         this.materials[this.defaultEdgeColor] = this.makeMaterial(this.defaultEdgeColor);
@@ -61,7 +66,7 @@ var igraph = {
 
     // Draws webgl meshes from input javascript object
     draw: function (graph) {
-        var material, mesh, mag, self, map, n1, n2;
+        var material, mesh, arrow, mag, self, map, n1, n2;
         self = this;
         map = {};
 
@@ -78,7 +83,9 @@ var igraph = {
             mesh = new THREE.Mesh(self.sphereGeometry, material);
             mesh.position.fromArray(node.hasOwnProperty("location") ?
                                     node.location : [0, 0, 0]);
-            mesh.scale.x = mesh.scale.y = mesh.scale.z = self.nodeSize;
+            if (node.hasOwnProperty("size")) {
+                mesh.scale.set(node.size, node.size, node.size);
+            }
             map[k] = self.nodes.length;
             self.scene.add(mesh);
             self.nodes.push(mesh);
@@ -99,13 +106,23 @@ var igraph = {
             mesh = new THREE.Mesh(self.cylinderGeometry, material);
             mesh.position.addVectors(n1.position, n2.position).divideScalar(2.0);
             mesh.lookAt(n2.position);
-            mesh.scale.x = mesh.scale.y = self.edgeSize;
-            mesh.scale.z = n2.position.distanceTo(n1.position);
+            mag = n2.position.distanceTo(n1.position);
+            mesh.scale.z = mag;
+
             // Save array-index references to nodes, mapping from object structure
             mesh.source = map[edge.source.toString()];
             mesh.target = map[edge.target.toString()];
             self.scene.add(mesh);
             self.edges.push(mesh);
+
+            // If directed, add on arrows
+            if (self.directed) {
+                arrow = new THREE.Mesh(self.coneGeometry, material);
+                arrow.position = mesh.position;
+                arrow.lookAt(n2.position);
+                self.scene.add(arrow);
+                self.arrows.push(arrow);
+            }
         });
 
         if (this.runOptimization) {
@@ -151,7 +168,7 @@ var igraph = {
     // Runs a force-directed layout algorithm on the currently displayed graph
     optimize: function (options) {
         var iterations, forceStrength, dampening, maxVelocity, maxDistance,
-            self, l, i, j, k, delta, mag, n1, n2, e;
+            iterate, self, l, i, j, k, delta, mag, n1, n2, e, a;
         self = this;
 
         options = options || {};
@@ -166,73 +183,81 @@ var igraph = {
             self.nodes[i].force = new THREE.Vector3();
         }
 
-        for (i = 0; i < iterations; i += 1) {
-            setTimeout(function () {
-                dampening -= 0.01 / iterations;
+        iterate = function () {
+            dampening -= 0.01 / iterations;
 
-                // Add in Coulomb-esque node-node repulsive forces
-                for (j = 0; j < self.nodes.length; j += 1) {
-                    for (k = 0; k < self.nodes.length; k += 1) {
-                        if (j === k) {
-                            continue;
-                        }
-                        n1 = self.nodes[j];
-                        n2 = self.nodes[k];
-
-                        delta.subVectors(n2.position, n1.position);
-                        mag = delta.length();
-                        if (mag < 0.1) {
-                            delta.set(Math.random(), Math.random(), Math.random())
-                                 .multiplyScalar(0.1).addScalar(0.1);
-                            mag = delta.length();
-                        }
-                        if (mag < maxDistance) {
-                            delta.multiplyScalar(forceStrength * forceStrength / (mag * mag));
-                            n1.force.sub(delta);
-                            n2.force.add(delta);
-                        }
+            // Add in Coulomb-esque node-node repulsive forces
+            for (j = 0; j < self.nodes.length; j += 1) {
+                for (k = 0; k < self.nodes.length; k += 1) {
+                    if (j === k) {
+                        continue;
                     }
-                }
-
-                // Add Hooke-esque edge spring forces
-                for (j = 0; j < self.edges.length; j += 1) {
-                    n1 = self.nodes[self.edges[j].source];
-                    n2 = self.nodes[self.edges[j].target];
+                    n1 = self.nodes[j];
+                    n2 = self.nodes[k];
 
                     delta.subVectors(n2.position, n1.position);
                     mag = delta.length();
                     if (mag < 0.1) {
-                        delta.set(THREE.Math.randFloat(0.1, 0.2),
-                                  THREE.Math.randFloat(0.1, 0.2),
-                                  THREE.Math.randFloat(0.1, 0.2));
+                        delta.set(Math.random(), Math.random(), Math.random())
+                             .multiplyScalar(0.1).addScalar(0.1);
                         mag = delta.length();
                     }
-                    mag = Math.min(mag, maxDistance);
-                    delta.multiplyScalar((mag * mag - forceStrength * forceStrength) / (mag * forceStrength));
-                    n1.force.add(delta);
-                    n2.force.sub(delta);
+                    if (mag < maxDistance) {
+                        delta.multiplyScalar(forceStrength * forceStrength / (mag * mag));
+                        n1.force.sub(delta.clone().multiplyScalar(n2.scale.x));
+                        n2.force.add(delta.clone().multiplyScalar(n1.scale.x));
+                    }
                 }
+            }
 
-                // Move by resultant force
-                for (j = 0; j < self.nodes.length; j += 1) {
-                    n1 = self.nodes[j];
-                    n1.force.multiplyScalar(dampening);
-                    n1.force.setX(THREE.Math.clamp(n1.force.x, -maxVelocity, maxVelocity));
-                    n1.force.setY(THREE.Math.clamp(n1.force.y, -maxVelocity, maxVelocity));
-                    n1.force.setZ(THREE.Math.clamp(n1.force.z, -maxVelocity, maxVelocity));
+            // Add Hooke-esque edge spring forces
+            for (j = 0; j < self.edges.length; j += 1) {
+                n1 = self.nodes[self.edges[j].source];
+                n2 = self.nodes[self.edges[j].target];
 
-                    n1.position.add(n1.force);
-                    n1.force.set(0, 0, 0);
+                delta.subVectors(n2.position, n1.position);
+                mag = delta.length();
+                if (mag < 0.1) {
+                    delta.set(THREE.Math.randFloat(0.1, 0.2),
+                              THREE.Math.randFloat(0.1, 0.2),
+                              THREE.Math.randFloat(0.1, 0.2));
+                    mag = delta.length();
                 }
-                for (j = 0; j < self.edges.length; j += 1) {
-                    e = self.edges[j];
-                    n1 = self.nodes[e.source];
-                    n2 = self.nodes[e.target];
-                    e.position.addVectors(n1.position, n2.position).divideScalar(2.0);
-                    e.lookAt(n2.position);
-                    e.scale.z = n2.position.distanceTo(n1.position);
+                mag = Math.min(mag, maxDistance);
+                delta.multiplyScalar((mag * mag - forceStrength * forceStrength) / (mag * forceStrength));
+                n1.force.add(delta.clone().multiplyScalar(n2.scale.x));
+                n2.force.sub(delta.clone().multiplyScalar(n1.scale.x));
+            }
+
+            // Move by resultant force
+            for (j = 0; j < self.nodes.length; j += 1) {
+                n1 = self.nodes[j];
+                n1.force.multiplyScalar(dampening);
+                n1.force.setX(THREE.Math.clamp(n1.force.x, -maxVelocity, maxVelocity));
+                n1.force.setY(THREE.Math.clamp(n1.force.y, -maxVelocity, maxVelocity));
+                n1.force.setZ(THREE.Math.clamp(n1.force.z, -maxVelocity, maxVelocity));
+
+                n1.position.add(n1.force);
+                n1.force.set(0, 0, 0);
+            }
+            for (j = 0; j < self.edges.length; j += 1) {
+                e = self.edges[j];
+                n1 = self.nodes[e.source];
+                n2 = self.nodes[e.target];
+                e.position.addVectors(n1.position, n2.position).divideScalar(2.0);
+                e.lookAt(n2.position);
+                e.scale.z = n2.position.distanceTo(n1.position);
+
+                if (self.directed) {
+                    a = self.arrows[j];
+                    a.position = e.position;
+                    a.lookAt(n2.position);
                 }
-            }, 0);
+            }
+        };
+
+        for (i = 0; i < iterations; i += 1) {
+            setTimeout(iterate, 0);
         }
     }
 };
